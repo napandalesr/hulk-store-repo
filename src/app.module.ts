0f390401f339c9459582f76126b71129a494564d
Praxis <neider.pandales@cafedecolombia.com>
2021-09-01 12:49:40 -0500

    database conexion

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,33 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DATABASE, HOST, PASSWORD, PORT, USERNAME } from './utils/constants';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'mysql',
+        host: config.get<string>(HOST),
+        port: parseInt(config.get<string>(PORT),10),
+        username: config.get<string>(USERNAME),
+        password: config.get<string>(PASSWORD),
+        database: config.get<string>(DATABASE),
+        entities: [__dirname+'./**/**/*entity{.js,.ts}'],
+        autoLoadEntities: true,
+        synchronize: true,
+        logging: true,
+        logger: 'file'
+      })
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env'
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/utils/constants.ts
@@ -0,0 +1,5 @@
+export const HOST = 'DATABASE_HOST';
+export const PORT = 'DATABASE_PORT';
+export const USERNAME = 'DATABASE_USERNAME';
+export const PASSWORD = 'DATABASE_PASSWORD';
+export const DATABASE = 'DATABASE_NAME';
